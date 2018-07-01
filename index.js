65b8bcbfabdb51e0d5f1230efe282a05984afc79
Thean Hoogenboezem <theunis.hoogenboezem@standardbank.co.za>
2018-07-01 18:51:30 +0200

    Weather app example code from terminal

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const request = require('request');
+const argv = require('yargs').argv;
 
 let apiKey = '8c9dc6a40ac1613486c1beb1971623c2';
-let city = 'Johannesburg';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+let city = argv.c || 'johannesburg';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 request(url, function (err, response, body) {
     if(err){
         console.log('error:', error);
     } else {
         console.log('body:', body);
+        try {
+            let weather = JSON.parse(body);
+            console.log("JSON ", weather);
+            let message = `It's ${weather.main.temp} degrees Celsius in ${weather.name}!`;
+            console.log(message);
+        } catch (e) {
+            console.log('Parsing Error!', e);
+        }
     }
 });
